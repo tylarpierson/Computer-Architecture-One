817a9bd22f0c9627f3979b5690a3614ee6ba7d80
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-06-14 12:43:14 -0700

    working on call/ret

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -8,6 +8,8 @@ const HLT = 0b00000001;
 const MUL = 0b10101010;
 const PUSH = 0b01001101;
 const POP = 0b01001100;
+const CALL = 0b01001000;
+const RET = 0b00001001;
 
 /**
  * Class for simulating a simple Computer (CPU & memory)
